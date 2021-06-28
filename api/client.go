5aad07c392fe80e6911d3b779870b44204b63f84
Ciarán Cotter <74498922+ciarancotter@users.noreply.github.com>
2021-06-28 12:12:03 +0100

    Update client.go

--- api/client.go
+++ api/client.go
@@ -8,81 +8,101 @@ import (
 	"net/http"
 )
 
-type CloudCIX_Client struct {
-	Email, Password, Api_key, Token, API_URL string
+type CloudCIXClient struct {
+	Email, Password, ApiKey, Token, ApiUrl string
 }
 
-func (cix_client CloudCIX_Client) Get_Token() string {
-
-	json_data := map[string]string{"api_key": cix_client.Api_key, "email": cix_client.Email, "password": cix_client.Password}
-	json_value, _ := json.Marshal(json_data)
-	url := "https://membership." + cix_client.API_URL + "/auth/login/"
-	response, err := http.Post(url, "application/json", bytes.NewBuffer(json_value))
-
+func (cixClient CloudCIXClient) GetToken() (string, error) {
+	json_data := map[string]string{"api_key": cixClient.ApiKey, "email": cixClient.Email, "password": cixClient.Password}
+	json_value, err := json.Marshal(json_data)
 	if err != nil {
-		return "Failed"
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		var HTTPresponse map[string]string
-		json.Unmarshal(data, &HTTPresponse)
-		return HTTPresponse["token"]
+		return "", err
 	}
+
+	url := fmt.Sprintf("https://membership.%s/auth/login/", cixClient.ApiUrl)
+	response, err := http.Post(url, "application/json", bytes.NewBuffer(json_value))
+	if err != nil {
+		return "", err
+	}
+
+	data, err := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+	if err != nil {
+		return "", err
+	}
+
+	var resp map[string]string
+	err = json.Unmarshal(data, &resp)
+	return resp["token"], err
 }
 
-func (cix_client CloudCIX_Client) Read_Data(application string, service string, object_id string, token string) {
+func (cixClient CloudCIXClient) ReadData(application string, service string, object_id string, token string) (interface{}, error) {
 	client := http.Client{}
-	url := "https://" + application + "." + cix_client.API_URL + "/" + service + "/" + object_id
-	request, _ := http.NewRequest("GET", url, nil)
+	url := "https://" + application + "." + cixClient.ApiUrl + "/" + service + "/" + object_id
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+
 	request.Header.Set("X-Auth-Token", token)
 	response, err := client.Do(request)
-
 	if err != nil {
-		fmt.Println("Failed")
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-
-		if err != nil {
-			fmt.Println("This data is not a JSON response.")
-		} else {
-			Application_switch(application, service, object_id, data, err)
-		}
-
+		return "", err
 	}
+
+	data, err := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+	if err != nil {
+		return "", err
+	}
+
+	return ApplicationSwitch(application, service, object_id, data)
 }
 
-func (cix_client CloudCIX_Client) Write_Data(application string, service string, object_id string, token string, data map[string]string, method string) {
+func (cixClient CloudCIXClient) WriteData(application string, service string, object_id string, token string, data map[string]string, method string) (string, error) {
 	client := http.Client{}
-	url := "https://" + application + "." + cix_client.API_URL + "/" + service + "/" + object_id
+	url := "https://" + application + "." + cixClient.ApiUrl + "/" + service + "/" + object_id
 	post_data, _ := json.Marshal(data)
 	post_data_buffer := bytes.NewBuffer(post_data)
-	request, _ := http.NewRequest(method, url, post_data_buffer)
+	request, err := http.NewRequest(method, url, post_data_buffer)
+	if err != nil {
+		return "", err
+	}
+
 	request.Header.Set("X-Auth-Token", token)
 	response, err := client.Do(request)
-
 	if err != nil {
-		fmt.Println("Failed")
-	} else {
-		response_data, _ := ioutil.ReadAll(response.Body)
-		fmt.Println(string(response_data))
+		return "", err
 	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
 }
 
-func (cix_client CloudCIX_Client) Delete_Data(application string, service string, object_id string, token string) {
+func (cix_client CloudCIXClient) DeleteData(application string, service string, object_id string, token string) (string, error) {
 	client := http.Client{}
-	url := "https://" + application + "." + cix_client.API_URL + "/" + service + "/" + object_id
-	request, _ := http.NewRequest("DELETE", url, nil)
+	url := "https://" + application + "." + cix_client.ApiUrl + "/" + service + "/" + object_id
+	request, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return "", err
+	}
+
 	request.Header.Set("X-Auth-Token", token)
 	response, err := client.Do(request)
-
 	if err != nil {
-		fmt.Println("Failed")
-	} else {
-		if err != nil {
-			fmt.Println("Did not delete.")
-		} else {
-			fmt.Println(response)
-			fmt.Println("Deleted successfully")
-		}
-
+		return "", err
 	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
 }
