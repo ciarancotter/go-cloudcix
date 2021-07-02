7f2effb9bff93854e84ef90f2d3be68d5f0aa31e
Ciarán Cotter <74498922+ciarancotter@users.noreply.github.com>
2021-07-02 14:51:26 +0100

    Go-SDK v2.0 (#4)
    * Go-SDK v2.0
    
    Redo of Go SDK.
    
    * Update README.md

--- api/membership.go
+++ api/membership.go
@@ -118,6 +118,117 @@ type Address struct {
 	} `json:"content"`
 }
 
+type AddressSpecific struct {
+	Content struct {
+		Address1         string `json:"address1"`
+		Address2         string `json:"address2"`
+		Address3         string `json:"address3"`
+		BillingAddressID int    `json:"billing_address_id"`
+		City             string `json:"city"`
+		CloudRegion      bool   `json:"cloud_region"`
+		Country          struct {
+			Alpha2Code       string `json:"alpha_2_code"`
+			Alpha3Code       string `json:"alpha_3_code"`
+			EnglishName      string `json:"english_name"`
+			ID               int    `json:"id"`
+			PhonePrefix      string `json:"phone_prefix"`
+			PrimaryLevelName string `json:"primary_level_name"`
+			URI              string `json:"uri"`
+		} `json:"country"`
+		Currency struct {
+			ID     int    `json:"id"`
+			Name   string `json:"name"`
+			Symbol string `json:"symbol"`
+			URI    string `json:"uri"`
+		} `json:"currency"`
+		Email       string `json:"email"`
+		FullAddress string `json:"full_address"`
+		Gln         string `json:"gln"`
+		ID          int    `json:"id"`
+		Language    struct {
+			Code        string `json:"code"`
+			EnglishName string `json:"english_name"`
+			ID          int    `json:"id"`
+			NativeName  string `json:"native_name"`
+			URI         string `json:"uri"`
+		} `json:"language"`
+		Link struct {
+			AddressID       int    `json:"address_id"`
+			Client          bool   `json:"client"`
+			Compute         bool   `json:"compute"`
+			ContraAddressID int    `json:"contra_address_id"`
+			CreditLimit     string `json:"credit_limit"`
+			Customer        bool   `json:"customer"`
+			Note            string `json:"note"`
+			Reference       string `json:"reference"`
+			ServiceCentre   bool   `json:"service_centre"`
+			Supplier        bool   `json:"supplier"`
+			Territory       struct {
+				ID     int `json:"id"`
+				Member struct {
+					APIKey   string `json:"api_key"`
+					Currency struct {
+						ID     int    `json:"id"`
+						Name   string `json:"name"`
+						Symbol string `json:"symbol"`
+						URI    string `json:"uri"`
+					} `json:"currency"`
+					GlnPrefix   string `json:"gln_prefix"`
+					ID          int    `json:"id"`
+					Name        string `json:"name"`
+					Secret      bool   `json:"secret"`
+					SelfManaged bool   `json:"self_managed"`
+					URI         string `json:"uri"`
+				} `json:"member"`
+				Name string `json:"name"`
+				URI  string `json:"uri"`
+			} `json:"territory"`
+			URI       string `json:"uri"`
+			Warrantor bool   `json:"warrantor"`
+		} `json:"link"`
+		Linked bool `json:"linked"`
+		Member struct {
+			APIKey   string `json:"api_key"`
+			Currency struct {
+				ID     int    `json:"id"`
+				Name   string `json:"name"`
+				Symbol string `json:"symbol"`
+				URI    string `json:"uri"`
+			} `json:"currency"`
+			GlnPrefix   string `json:"gln_prefix"`
+			ID          int    `json:"id"`
+			Name        string `json:"name"`
+			Secret      bool   `json:"secret"`
+			SelfManaged bool   `json:"self_managed"`
+			URI         string `json:"uri"`
+		} `json:"member"`
+		Name   string `json:"name"`
+		Phones []struct {
+			Name   string `json:"name"`
+			Number string `json:"number"`
+		} `json:"phones"`
+		Postcode    string `json:"postcode"`
+		Subdivision struct {
+			AlphaCode string `json:"alpha_code"`
+			Country   struct {
+				Alpha2Code       string `json:"alpha_2_code"`
+				Alpha3Code       string `json:"alpha_3_code"`
+				EnglishName      string `json:"english_name"`
+				ID               int    `json:"id"`
+				PhonePrefix      string `json:"phone_prefix"`
+				PrimaryLevelName string `json:"primary_level_name"`
+				URI              string `json:"uri"`
+			} `json:"country"`
+			EnglishName string `json:"english_name"`
+			ID          int    `json:"id"`
+			URI         string `json:"uri"`
+		} `json:"subdivision"`
+		URI       string `json:"uri"`
+		VatNumber string `json:"vat_number"`
+		Website   string `json:"website"`
+	} `json:"content"`
+}
+
 type AppSettings struct {
 	Content struct {
 		Created        string `json:"created"`
@@ -908,7 +1019,7 @@ type TransactionType struct {
 	} `json:"content"`
 }
 
-type TranscationTypeSpecific struct {
+type TransactionTypeSpecific struct {
 	Content struct {
 		ID   int    `json:"id"`
 		Name string `json:"name"`
